40cd95842ab0c5b7f2ce40037e6d8eb3720dd46c
Jordan <jordancolgan@gmail.com>
2015-11-21 16:42:08 +0000

    Allow dynamic setting of title for routes

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -5,9 +5,14 @@
     .module('curoPortal')
     .run(runBlock);
 
+  runBlock.$inject = ['$log', '$rootScope', '$route'];
+
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope, $route) {
     $log.debug('runBlock end');
+    $rootScope.$on('$routeChangeSuccess', function() {
+      document.title = $route.current.title;
+    });
   }
 
 })();
