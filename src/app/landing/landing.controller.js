dfa4f997d35c8816a8c921ffbd28c4c0f38018cc
Jordan <jordancolgan@gmail.com>
2015-11-21 18:31:00 +0000

    [WIP] Update landing page
    - Remove redundant CSS
    - Add links to other resources
    - Remove user login call from landing controller

--- src/app/landing/landing.controller.js
+++ src/app/landing/landing.controller.js
@@ -6,12 +6,7 @@
     .controller('LandingController', LandingController);
 
   /** @ngInject */
-  function LandingController($log, ApplicationUser) {
+  function LandingController($log) {
     var controller = this;
-    controller.text = "Hello";
-    ApplicationUser.login({username: 'neil.armstrong', password: 'password'}, function(err, accessToken) {
-      $log.log(err);
-      $log.log(accessToken);
-    });
   }
 })();
