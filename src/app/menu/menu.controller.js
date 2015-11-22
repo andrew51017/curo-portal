a8e47a982fdfbbac160e392ea57d94ddbf66de16
Jordan <jordancolgan@gmail.com>
2015-11-22 13:40:27 +0000

    [WIP] Add menu page

---
+++ src/app/menu/menu.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('curoPortal')
+    .controller('MenuController', MenuController);
+
+  /** @ngInject */
+  function MenuController($log) {
+    var controller = this;
+  }
+})();
