2b6afa079b1a2560fa46ab517a4edbedee2edb98
Jordan <jordancolgan@gmail.com>
2015-11-22 12:43:19 +0000

    [WIP] Add alerts for login

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -6,14 +6,15 @@
         .controller('LoginController', LoginController);
 
     /** @ngInject */
-    function LoginController($log, $location, ApplicationUser) {
+    function LoginController($log, $location, $scope, ApplicationUser) {
         var controller = this;
+        $scope.alerts = [];
         controller.login = function (username, password) {
             ApplicationUser.login({username: username, password: password}, function(err, accessToken) {
                 if (accessToken !== undefined) {
                     $location.path('/dashboard');
                 } else {
-                    $log.log('Could not authenticate');
+                    // display error
                 }
             });
         };
