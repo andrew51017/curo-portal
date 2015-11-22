bcec1a422342c1c0c1c7f8433e6616893d0b97b2
Jordan <jordancolgan@gmail.com>
2015-11-22 10:26:50 +0000

    Add angular-google-places-autocomplete

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,5 +1,18 @@
 (function() {
   'use strict';
 
-  angular.module('curoPortal', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ngRoute', 'ui.bootstrap', 'lbServices', 'door3.css', 'ui.calendar']);
+  angular.module('curoPortal', [
+    'ngAnimate',
+    'ngCookies',
+    'ngTouch',
+    'ngSanitize',
+    'ngMessages',
+    'ngAria',
+    'restangular',
+    'ngRoute',
+    'ui.bootstrap',
+    'lbServices',
+    'door3.css',
+    'ui.calendar',
+    'google.places']);
 })();
