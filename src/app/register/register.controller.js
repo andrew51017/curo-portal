2ea35281daeb50f7cf063eda60f8a11baa1e0765
Jordan <jordancolgan@gmail.com>
2015-11-22 14:47:06 +0000

    Update registration
    - Add alert
    - Add redirect on success

--- src/app/register/register.controller.js
+++ src/app/register/register.controller.js
@@ -6,12 +6,20 @@
     .controller('RegisterController', RegisterController);
 
   /** @ngInject */
-  function RegisterController($log, ApplicationUser) {
+  function RegisterController($log, $location, UserResource, ApplicationUser) {
     var controller = this;
+    controller.alerts = [];
 
     controller.register = function() {
       controller.user.username = controller.user.email;
-      ApplicationUser.create(controller.user);
+      ApplicationUser.create(controller.user, function(response) {
+        if (response.user !== undefined) {
+          UserResource.setUser(response.user);
+          $location.path('/dashboard');
+        }
+      }, function() {
+        controller.alerts.push({type: 'danger', msg: 'Could not create an account, please try again.'});
+      });
     }
   }
 })();
