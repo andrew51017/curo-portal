b60677dedc7a020e17dd4a609781805ab3f92506
neil_armstrong_1991@hotmail.com <neil@Neils-MacBook-Pro.local>
2015-11-22 11:00:30 +0000

    Added sidebar directive.

--- src/app/components/sidebar/sidebar.directive.js
+++ src/app/components/sidebar/sidebar.directive.js
@@ -3,30 +3,30 @@
 
   angular
     .module('curoPortal')
-    .directive('directiveExample', directiveExample);
+    .directive('sideBar', sideBar);
 
   /** @ngInject */
-  function directiveExample() {
-    var directive = {
+  function sideBar() {
+    return {
       restrict: 'E',
       scope: {
         // Add scope values here, defined like <my-directive my-value="blah"/>
         // Note that camel-cased scopes defined here are represented differently in html (myDirective becomes my-directive).
       },
-      template: '&nbsp;',
+      templateUrl: 'app/components/sidebar/sidebar.html',
       link: linkFunc,
       controller: Controller,
       controllerAs: 'controller'
     };
-
-    return directive;
 
     function linkFunc(scope, el, attr, controller) {
 
     }
 
     /** @ngInject */
-    function Controller(/* Inject dependencies here */) {
+    function Controller() {
+      var controller = this;
+      controller.user = {name: 'Test McBob'};
 
     }
   }
--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -8,5 +8,25 @@
   /** @ngInject */
   function DashboardController($log) {
     var controller = this;
+
+    // TODO: Replace with api call
+    controller.bookings = {
+      events: [
+        {
+          title: 'Holiday Inn Christmas party',
+          start: '2015-11-23'
+        },
+        {
+          title: 'Chain reaction Christmas party',
+          start: '2015-11-24'
+        }
+      ]
+    };
+
+    //controller.uiConfig = {
+    //  calendar: {
+    //
+    //  }
+    //}
   }
 })();
