5242d0fd44d50f60d8da161255361d8a32f9e158
Jordan <jordancolgan@gmail.com>
2015-11-22 13:39:45 +0000

    [WIP] Add bookings page

---
+++ src/app/bookings/bookings.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('curoPortal')
+    .controller('BookingsController', BookingsController);
+
+  /** @ngInject */
+  function BookingsController($log) {
+    var controller = this;
+  }
+})();
